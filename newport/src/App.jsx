e690d3930950f7edd804448eabe403f165733034
alurubalakarthikeya <alurubalakarthikeya@gmail.com>
2025-08-24 12:54:21 +0530

    ui

--- newport/src/App.jsx
+++ newport/src/App.jsx
@@ -11,6 +11,9 @@ function App() {
           <h2>Hi, I’m Karthikeya</h2>
           <h4>A Frontend Developer, UI/UX Designer, Agent Developer</h4>
         </div>
+        <div className="profile-pictur">
+          <img src="/src/assets/imgs/profile-pic.jpg" alt="Profile" className="profile-img" />
+        </div>
       </div>
     </>
   );
